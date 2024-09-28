d9d9479dbdfc12942ba9c53a0d5e5d0955f7f0a2
Anubhav Paras <aparas@zoox.com>
2024-09-28 14:20:42 -0700

    renamed the types.py file

--- streamlit/tutorial_widget.py
+++ streamlit/tutorial_widget.py
@@ -3,9 +3,7 @@ from typing import List, Optional
 import numpy as np
 from dataclasses import dataclass
 
-from your_friendly_ai_teacher.types import VideoInstructionGeneration
-
-
+from your_friendly_ai_teacher.custom_types import VideoInstructionGeneration
 
 
 def call_llm(
@@ -22,7 +20,9 @@ def get_videos_from_instructions(tutorial_instructions: List[str]) -> List[Video
     Call Luma AI API to get the generated.
     """
     # TODO (sakshi): Integrate the call to get the generated video.
-    return [VideoInstructionGeneration()]
+    dummy_instruction = VideoInstructionGeneration(id="1", url="https://storage.cdn-luma.com/lit_lite_inference_v1.6-xl/f3e0893e-9f22-42bb-87ef-30f0a78642f1/46f24df2-c6e8-42df-aead-9a4058e406df_video0d8e5285574c94fcc88e8c929964805de.mp4", tutorial_instruction="A teddy bear in sunglasses playing electric guitar and dancing")
+    
+    return [dummy_instruction, dummy_instruction, dummy_instruction]
 
 
 
@@ -55,12 +55,17 @@ def render_tutorial_instructions(
     :param image_inp: image-based user input.
     """
 
-    tutorial_instructions = get_tutorial_instructions(
-        role=role, task_query=task_query, image_inp=image_inp
-    )
-    video_instructions = get_videos_from_instructions(tutorial_instructions)
+    with st.spinner("Hmmm! Let me think how can I help you... :thinking_face:"):
+    
+        tutorial_instructions = get_tutorial_instructions(
+            role=role, task_query=task_query, image_inp=image_inp
+        )
+        video_instructions = get_videos_from_instructions(tutorial_instructions)
 
     for video_instruction in video_instructions:
-        pass 
+        st.write(video_instruction.tutorial_instruction)
+        st.video(video_instruction.url)
+
+         
 
 
